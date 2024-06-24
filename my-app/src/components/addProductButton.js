261510dc2cccb43c34008a805d66aa652fb395d2
Gautam Singh <gautam.singh01@ad.infosys.com>
2024-06-24 17:43:12 +0530

    Added Basic functionalities like input dialog box and some styling

--- my-app/src/components/addProductButton.js
+++ my-app/src/components/addProductButton.js
@@ -1,8 +1,11 @@
+import { Button } from '@mui/material'
 import React from 'react'
 
 const AddProductButton = () => {
   return (
-    <div>AddProductButton</div>
+    <div style={{position:'absolute', top:'80px',left:'600px'}}>
+        <Button variant='outlined' sx={{color:'#1b5e20',width:'250px', borderColor:'#1b5e20',borderWidth:'2px'}}>Add Product</Button>
+    </div>
   )
 }
 
--- my-app/src/components/productList.js
+++ my-app/src/components/productList.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react'
-import {Select, TextField, IconButton,InputAdornment, Button} from '@mui/material'
-import { MenuItem } from '@mui/material'
+import {Select, TextField, IconButton,InputAdornment, Button,MenuItem,Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions} from '@mui/material'
 import EditIcon from '@mui/icons-material/Edit'
+import CloseIcon from '@mui/icons-material/Close';
+import SearchIcon from '@mui/icons-material/Search';
 import {ProductPicker} from './productPicker'
+import AddProductButton from './addProductButton'
 
 const ProductList = () => {
     const [product,setProduct]=useState('');
     const [addMode,setAddMode]=useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
     const [pickerStatus,SetPickerStatus]= useState(false);
     const [discountType,setDiscountType]=useState('% OFF');
 
@@ -22,8 +28,16 @@ const ProductList = () => {
     const handleAddDiscount = ()=>{
         setAddMode(true);
     }
+    const handleClose = ()=>{
+        SetPickerStatus(false)
+    }
+    const handleSearch = () => {
+        console.log('Searching for:');
+        // Implement your search functionality here
+        handleClose();
+      };
   return (
-    ( addMode?(<>
+    ( addMode?(<div style={{position:'relative'}}>
         <TextField
         value={product}
         placeholder="Select a product"
@@ -43,7 +57,7 @@ const ProductList = () => {
       />
         <TextField
         type='number'
-        sx={{width:'80px'}}
+        sx={{width:'80px',margin:'5px'}}
         />
         <Select
         value={discountType}
@@ -52,8 +66,49 @@ const ProductList = () => {
             <MenuItem value='% OFF'>% OFF</MenuItem>
             <MenuItem value='Flat OFF'>Flat OFF</MenuItem>
         </Select>
-        </>)
-        :(<>
+        <Dialog open={pickerStatus} onClose={handleClose} fullWidth maxWidth="sm">
+        <DialogTitle>
+          Search
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            sx={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="search"
+            label="Search term"
+            type="search"
+            fullWidth
+            variant="outlined"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                handleSearch();
+              }
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleSearch} variant="contained" endIcon={<SearchIcon />}>
+            Search
+          </Button>
+        </DialogActions>
+      </Dialog>
+        <AddProductButton/>
+        </div>)
+        :(<div style={{position:'relative'}}>
         <TextField
         value={product}
         placeholder="Select a product"
@@ -71,11 +126,52 @@ const ProductList = () => {
           ),
         }}
         />
+        <Dialog open={pickerStatus} onClose={handleClose} fullWidth maxWidth="sm">
+        <DialogTitle>
+          Search
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            sx={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="search"
+            label="Search term"
+            type="search"
+            fullWidth
+            variant="outlined"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                handleSearch();
+              }
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleSearch} variant="contained" endIcon={<SearchIcon />}>
+            Search
+          </Button>
+        </DialogActions>
+      </Dialog>
         <Button 
         variant='contained'
         onClick={handleAddDiscount}
-        sx={{height:'55px',padding:'25px'}}>Add Discount</Button>
-        </>)
+        sx={{height:'55px',margin:'5px'}}>Add Discount</Button>
+        <AddProductButton/>
+        </div>)
     )
   )
 }
